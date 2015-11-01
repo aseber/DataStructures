862b1fe40c82dac467f3b8d75f6dfe78814a9efe
aseber <aseber@techsouth.cc>
2015-11-01 06:59:07 -0500

    Adding min priority queues

---
+++ min_priority_heap_queue/min_priority_heap_queue.h
@@ -0,0 +1,25 @@
+/*
+*
+* Name: Austin Seber
+*
+* UFID: 01516234
+*
+* Gator ID: aseber
+*
+* Discussion Section: 13A8
+*
+*/
+
+// Constructor
+// Destructor
+// isEmpty
+// empty
+// size
+// push
+// top
+// pop
+
+template<typename T>
+class min_priority_heap_queue {
+
+}
